215db663e9965448eb3dc5b08ed8c967179f5104
Esmada Stevens <esmadastevenszimba@gmail.com>
2022-04-07 13:10:35 -0700

    Edited 4-puts.c using Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - prints the statement "Programming is like building a multilingual puzzle
- *Return: 0 (Success)
- *  
+ * main - Prints statement "Programming is like building a multilingual puzzle"
+ *
+ * Return: 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	return (0);
 }
