48b5538b32a3ae1a27ef6796e234f36334507851
Esmada Stevens <esmadastevenszimba@gmail.com>
2022-04-15 14:29:50 -0700

    Added 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 
 /**
  * _isupper - checks for uppercase character
- * @c: character to be checked
+ * @c: - value to be checked
  * Return: 1 if c is uppercase 0 otherwise
  */
 int _isupper(int c)
