9f84f48a264b6eb48773ae6136d1f5181ace4cf5
Esmada Stevens <esmadastevenszimba@gmail.com>
2022-04-21 13:04:27 -0700

    Edited 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,8 +3,8 @@
 
 /**
  * print_array - function that prints n elements of an array of integers
- * @a: 
- * @n:
+ * @a: pointer to be changed
+ * @n: integer
  * Return: Always 0
  */
 void print_array(int *a, int n)
