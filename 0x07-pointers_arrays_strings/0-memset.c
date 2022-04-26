7ea3fe860489e9fb3506700888e284750cd8b725
Esmada Stevens <esmadastevenszimba@gmail.com>
2022-04-26 11:55:45 -0700

    Added 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_memset - function that fills memory with a constant byte
+ * @s: memory to be filled
+ * @b: value to be copied
+ * @n: number of times to copy b
+ * Return: pointer to memory s
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+Even more pointers, arrays, strings
+Write a function that fills memory with a constant byte
